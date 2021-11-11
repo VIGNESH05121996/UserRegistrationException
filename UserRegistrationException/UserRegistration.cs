bf4bd311914e793a8e0540d60077128d4bf7b786
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-11 18:16:03 +0530

    [VIGNESH M] Added: FirstName Method and Test cases

---
+++ UserRegistrationException/UserRegistration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationException
+{
+    public class UserRegistration
+    {
+        //UserRegistration Implementation
+       
+        public string FirstName(string name)
+        {
+            string firstName = "^[A-Z]{1}[a-z]{2,}$"; //Regular Expression shows that firstName starts with caps and minimum of 3 character
+            Regex regex = new Regex(firstName); //calling Regex function
+
+            try
+            {
+                if (regex.IsMatch(name))
+                {
+                    return "Valid";
+                }
+                else
+                {
+                    return "Invalid";
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "invalid";
+            }
+               
+
+        }
+
+    }
+}
---
+++ UserRegistrationTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationException;
+
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string expected = "Valid";
+
+            //Act
+            UserRegistration test = new UserRegistration();
+            string actual = test.FirstName("Vignesh");
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
